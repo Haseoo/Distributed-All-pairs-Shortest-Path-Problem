bc821127ee5e1200e705768c485cbfbf898ec110
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2021-10-28 15:31:23 +0200

    Remove uncesseary files

--- com.Github.Haseoo.DASPP.CoreData/Dtos/WorkerHostInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace com.Github.Haseoo.DASPP.CoreData.Dtos
-{
-    public class WorkerHostInfo
-    {
-        public string Uri { get; set; }
-        public int CoreCount { get; set; }
-    }
-}
--- com.Github.Haseoo.DASPP.Worker/Helpers/Element.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace com.Github.Haseoo.DASPP.Main.Helpers
-{
-    public class Element
-    {
-        public int Distance { get; set; }
-        public bool IsCounted { get; set; }
-        public int Previous { get; set; }
-    }
-}
